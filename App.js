90180c7abe4d7bb794acec0627fd7e4ce2218ab4
luanesouza <luanesouza411@gmail.com>
2020-04-25 14:40:53 -0400

    sets timer and reset button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -13,8 +13,9 @@ let time = {
 }
 
 export default function App() {
-  [timer, setTimer] = useState(time.seconds);
+  [timer, setTimer] = useState(0);
   [clicked, wasClicked] = useState(false);
+  [reset, resetTimer] = useState(false);
 
   [seconds, setSeconds] = useState(0);
   [minutes, setMinutes] = useState(0);
@@ -26,33 +27,39 @@ export default function App() {
   })
 
   const handlePress = () =>  {
-
     wasClicked(!clicked)
-
   }
 
   const _startTimer = () => {
     setTimeout(() => {
-
       setSeconds(seconds+1)
-      // time.seconds = timer;
+
+      if(seconds === 60){
+        setSeconds(0)
+        setMinutes(minutes + 1)
+      }
     }, 1000);
   }
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Timer: </Text>
+      <Text style={styles.welcome}> Timer </Text>
       {/*<Text style={styles.instructions}>{timer}</Text>*/}
-      <Text style={styles.instructions}>{minutes} : {seconds}</Text>
+      <Text style={styles.instructions}>{minutes < 10 ? `0${minutes}` : minutes} : {seconds < 10 ? `0${seconds}`: seconds}</Text>
       {/*<Text style={styles.instructions}></Text>*/}
-      <Button onPress={ () => handlePress()} title={clicked ? 'Pause' : 'Start Timer'}/>
+      <Button style={styles.button} onPress={ () => handlePress()} title={clicked ? 'Pause' : 'Start Timer'}/>
+      <Button onPress={ () => {setMinutes(0); setSeconds(0);}} title={seconds > 0 && !clicked ? 'Reset' : ''}/>
       <Text style={styles.instructions}>{instructions}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    width: 100,
+    height: 100,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
